46131cdec86bc9279c69cf4980e9b640fe35592e
redracame <linukaauchithya@gmail.com>
2025-01-22 17:00:25 +0530

    "📝 Add getReviews function and route to get reviews"

--- route/reviewRouter.js
+++ route/reviewRouter.js
@@ -2,8 +2,10 @@
 
 import express from 'express';
 import { addReview } from '../contol/reviewController.js';
+import { getReviews } from '../contol/productController.js';
 
 const reviewRouter = express.Router();
 reviewRouter.post("/", addReview);
+reviewRouter.get("/",getReviews)
 
 export default reviewRouter;
